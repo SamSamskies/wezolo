016edfcffca7e4972e5d7757678de3b43206ece0
fmackojc <fab.mackojc@gmail.com>
2013-05-23 14:32:28 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wezolo::Application.config.secret_token = 'd21768f7695ae119b0582b214662bf7a505d16d685311212304a071e477473647efd5be7d463e467e964e37f3363f43a443fdec790e42d45da290274bcae85c3'
