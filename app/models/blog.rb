ff56dc88a31caf74593d528a88aca2cbc420aecf
fmackojc <fab.mackojc@gmail.com>
2013-05-24 00:14:05 -0700

    create dependency to make blog posts delete when their blog is deleted, update seed file

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ActiveRecord::Base
-  has_many :posts
+  has_many :posts, :dependent => :destroy
   belongs_to :blog_host
   belongs_to :user
   attr_accessible :title, :url, :external_id, :user_id
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'CSV'
 CSV.foreach("./db/countries.csv") do |row|
   Country.create(name: row.first)
@@ -34,16 +27,17 @@ end
 
 
 blogger = BlogHost.create(name: 'Blogger')
+blogger = BlogHost.first
 user_ids = User.pluck(:id)
 100.times do
   blog = blogger.blogs.create(title: Faker::Company.catch_phrase, 
-                       url: 'http://' + Faker::Lorem.word + '.blogpost.com', 
+                       url: 'http://' + Faker::Lorem.characters(10) + '.blogspot.com', 
                        external_id: Array.new(7){rand 10}.join,
                        user_id: user_ids.sample)
   10.times do
     blog.posts.create(title: Faker::Company.catch_phrase,
-              body: Faker::Lorem.paragraph,
-              published_at: (1..365).to_a.sample.days.ago)
+                      body: Faker::Lorem.paragraph,
+                      published_at: (1..365).to_a.sample.days.ago)
   end
 end
 
