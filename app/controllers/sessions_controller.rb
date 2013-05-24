a7bb2ba668fa366f053e774a52a08d1560233fa1
sidazhang <sidazhang89@gmail.com>
2013-05-23 19:41:18 -0700

    removed deugging statements

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,9 @@
 class SessionsController < ApplicationController
 
   def create
-    p params
-    p current_user
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      p "signedin!"
       flash[:notice] = "Logged in!"
       redirect_to root_url
     else
