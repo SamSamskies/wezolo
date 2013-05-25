76158cb0cb85d9154bba884945315ea4566ca5f2
sidazhang <sidazhang89@gmail.com>
2013-05-25 01:36:29 -0700

    refactored follow link into a decorator; significant improvement in code readability

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -10,7 +10,23 @@ class UserDecorator < Draper::Decorator
   #     end
   #   end
 
-  def follow_link
-    # if h.current_user && 
+  def follow_link(followable_obj)
+    if h.current_user && h.current_user.user_followings_by_type[followable_obj.class.to_s].include?(followable_obj.id)
+      h.link_to "unfollow #{followable_obj.class.to_s.downcase}", h.follows_path(:followable_id => followable_obj.id, :followable_type => followable_obj.class.to_s), :method => :delete
+    else
+      h.link_to "follow #{followable_obj.class.to_s.downcase}", h.follows_path(:followable_id => followable_obj.id, :followable_type => followable_obj.class.to_s), :method => :post
+    end
   end
 end
+
+  #   - if current_user && current_user.user_followings_by_type["User"].include?(user.id)
+  #     %li= link_to "unfollow user", follows_path(:followable_id => user.id, :followable_type => user.class.to_s), :method => :delete
+  #   - else
+  #     %li= link_to "follow user", follows_path(:followable_id => user.id, :followable_type => user.class.to_s), :method => :post
+  # def downvote_button
+  #   if h.current_user.nil? || h.user_votes[object.id] != "down"
+  #     vote_button("Downvote", {:id => "down#{object.id}", :value => "down", :class => "btn vote"})
+  #   else
+  #     vote_button("Downvote", {:id => "no_down#{object.id}", :value => "no_down", :class => "btn vote btn-success"})
+  #   end
+  # end
