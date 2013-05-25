1caec5764e5516aec6bb82edbda15339d4319478
stephenitis <nguyenhstephen@gmail.com>
2013-05-25 00:10:15 -0700

    removed comments from search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,11 +8,6 @@ class SearchController < ApplicationController
    # Tire.index('countries,users').search
     #   query do
     #     string (params[:search])
-    #   end
-    # end
     @results = Country.search(params[:search], :load => true)
-    # @results = results.sort_by { |r| r._score }.reverse
-    # ids = results.map(&:id)
-    # @results = Country.find(results.map(&:id))
   end
 end
