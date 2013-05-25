337c32c1d58749b49093e706d109d7de7b77193d
sidazhang <sidazhang89@gmail.com>
2013-05-25 13:43:55 -0700

    refactored decorators and attempted to add tests to decorators

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe UserDecorator do
+
+
   context "#follow_link" do
     let!(:justin_bieber) { create(:user, :name => "justin bieber")}
-    let!(:usa) { create(:user, :name => "usa") }
-    it "see follow link" do
-
+    let!(:usa) { create(:country, :name => "usa") }
+    let!(:fan) { create(:user, :name => "fan") }
+    it "can see follow link if they have not followed the obj" do
+      pending
+      # fan.heroes << justin_bieber
+      # fan.following_countries << usa
+      # decorated_fan = fan.decorate
+      # decorated_fan.follow_link(usa).should eq(2)
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-
+require 'draper/test/rspec_integration'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
@@ -35,4 +35,9 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  #draper testing view context
+  Draper::ViewContext.test_strategy :fast do
+    include ApplicationHelper
+  end
 end
