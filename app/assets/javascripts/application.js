906e4f7a0b29b1fd4aadbbfdc6f65ef84a733570
sidazhang <sidazhang89@gmail.com>
2013-05-25 20:05:30 -0700

    styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,16 +17,4 @@
 //= require_tree .
 
 
-$(document).ready(function(){
-  $("#loginModal").on('ajax:error', function(event, xhr, status) {
-    $(".alert.alert-error").text($.parseJSON(xhr.responseText).error);
-  });
 
-  $("#loginModal").on('ajax:success', function(event, xhr, status) {
-    $("#loginModal").modal("hide");
-    window.location = "/home";
-  });
-
-
-
-});
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,17 @@
 $(document).ready(function(){
+  $("#loginModal").on('ajax:error', function(event, xhr, status) {
+    $(".alert.alert-error").text($.parseJSON(xhr.responseText).error);
+  });
+
+  $("#loginModal").on('ajax:success', function(event, xhr, status) {
+    $("#loginModal").modal("hide");
+    window.location = "/home";
+  });
+
+  $("#signupModal").on('ajax:success', function(event, xhr, status) {
+    $("#signupModal").modal("hide");
+    window.location.replace("/home")
+  });
+
 });
+
