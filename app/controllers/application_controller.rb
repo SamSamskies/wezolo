d92a0d169b4a0d378fd34341b2906baffc0f411b
sidazhang <sidazhang89@gmail.com>
2013-05-26 17:21:13 -0700

    incomplete users will be redirected to add involvement upon clicking to follow a user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,10 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
     # if user is not auhorized to create a follow they are redirected to involvement page to complete profile
-    if exception.action == :create && exception.subject.class == Follow
-      redirect_to new_involvement_path, flash[:notice] => exception.message
+    if exception.action == :create && exception.subject.class == Follow && current_user.auth_status == "incomplete"
+      redirect_to new_involvement_path, :notice => "Please add at least one involvement to complete your profile!"
     else
-      redirect_to root_path
+      redirect_to root_path, :notice => exception.message
     end
   end
 
