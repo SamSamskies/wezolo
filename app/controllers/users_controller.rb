44e207bc56f31c7fcc0fdecc6346b5aa7d344b95
fmackojc <fab.mackojc@gmail.com>
2013-05-27 09:54:43 -0700

    improved how the JS redirects after login or signup, extracted a method in the auth module

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
     user = User.new(email: params[:email], password: params[:password],password_confirmation: params[:password_confirmation], name: params[:name], status: params[:status].first)
     if user.save
       user.create_profile
-      session[:user_id] = user.id
+      set_session(user)
+      render :json => {:redirect => "/home"}
     else
       @error = user.errors.full_messages
       render :json => {:error => @error}, :status => :unprocessable_entity
