df8cfe532dc866642de95918622a83ef3ea5b925
Fab Mackojc <fab.mackojc@gmail.com>
2013-05-27 15:44:28 -0700

    add capybara helper to stub current user

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,10 +10,10 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
-  # config.include CapybaraHelpers
+  config.include CapybaraHelpers
   config.include Capybara::DSL
   Capybara.default_driver = :selenium
-  
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
