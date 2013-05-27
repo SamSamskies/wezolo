da916ffa64f3597486bd065dfb70b1c6ef9088d0
stephenitis <nguyenhstephen@gmail.com>
2013-05-26 23:18:16 -0700

    temporarily removed tire modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base  
-  include Tire::Model::Search
-  include Tire::Model::Callbacks
+  # include Tire::Model::Search
+  # include Tire::Model::Callbacks
 
   has_secure_password
 
