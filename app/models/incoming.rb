a4d6849afbd4ed9477d7302b048e1a5077585a27
sidazhang <sidazhang89@gmail.com>
2013-05-27 16:45:19 -0700

    removed old migrations

--- app/models/incoming.rb
+++
@@ -1,5 +0,0 @@
-class Incoming < ActiveRecord::Base
-  belongs_to :user
-  has_many :outgoings
-  attr_accessible :message, :status
-end
--- app/models/outgoing.rb
+++
@@ -1,5 +0,0 @@
-class Outgoing < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :incoming
-  attr_accessible :message, :user
-end
