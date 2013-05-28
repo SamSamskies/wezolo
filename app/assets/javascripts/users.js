16a6c2a69218595ab14f223aa39e77f7959f1a6a
stephenitis <nguyenhstephen@gmail.com>
2013-05-27 20:09:16 -0700

    fixed bug in user.js file that broke test

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,11 +1,3 @@
 
 $(document).ready(function(){
-    
-  
-    // Tags Input
-    // $(".tagsinput").tagsInput();
 });
-
-if window.location.indexOf("select_tumbler_blog") {
-  popTumblerModal();
-}
