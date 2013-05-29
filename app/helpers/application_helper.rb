22c38e73eda19ce6e705f207dc2830116994d2fe
fmackojc <fab.mackojc@gmail.com>
2013-05-29 01:42:20 -0700

    Fix some bugs with avatar helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper  
-  def avatar_url(user, size)
+  def avatar_url(user)
     if user.profile.photo_url.present?
       user.profile.photo_url
     else
       default_url = "#{root_url}images/avatar_default.png"
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI.escape(default_url)}"
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#200&d=#{CGI.escape(default_url)}"
     end
   end
 end
