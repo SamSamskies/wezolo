2f85b6c0710374636cdebf8e4383b06178c11106
sidazhang <sidazhang89@gmail.com>
2013-05-29 00:39:57 -0700

    fixed search integration test

--- spec/requests/discovery_spec.rb
+++ spec/requests/discovery_spec.rb
@@ -2,14 +2,16 @@ require 'spec_helper'
 
 describe "Discovery" do
   context "search" do
-    it "user can be searched" do
+    before do
       User.index.delete
       User.index.create
+      User.index.refresh
+    end
+    it "user can be searched" do
       user = create(:user, name: "justin bieber")
       country = create(:country, name: "country")
       involvement = create(:involvement, user: user, country: country)
       profile = create(:profile, user: user)
-      User.index.refresh
       page.set_rack_session(:user_id => user.id)
       visit '/'
       find('#search').set("justin bieber")
