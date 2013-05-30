8105138c74d075d8e1d80e106df051ab609c00b4
sidazhang <sidazhang89@gmail.com>
2013-05-30 00:07:44 -0700

    added update auth status call to authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,9 @@ class ApplicationController < ActionController::Base
     AuthProvider.find_by_name(auth_provider_name).id
   end
 
+  # refactoring required
   rescue_from CanCan::AccessDenied do |exception|
+    current_user.update_auth_status if current_user.auth_status == "incomplete" && current_user.status == "interested"
     if current_user.present? && exception.action == :create && exception.subject.class == Follow && current_user.auth_status == "incomplete"
       redirect_to new_involvement_path, :notice => "Please complete your profile before you can follow other users!"
     elsif current_user.present? && exception.action == :read && exception.subject.class == UserDecorator && current_user.auth_status == "incomplete"
