d8d79fb0b7b75518d0b849329f9b0de94c329e99
Myles Byrne <myles@myles.id.au>
2013-06-04 16:29:54 -0700

    Generate boilerplate rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wezolo::Application.config.secret_token = '655aaecb2b5f05e7c86a6d6373ab34b4b51fd758107689312990a9e8d870137bb265ae4f3c58e02e571e17c3ee02eb57e0dc12129f928850b7d0a202a5b4c314'
