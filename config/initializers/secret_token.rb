b95f7af4ebbc895d23c2b2dd5b838554813019a2
sidney zhang <sidazhang89@gmail.com>
2013-06-04 21:35:26 -0700

    Rails Boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wezolo::Application.config.secret_token = '28794908c37a43feb875dc857816881b624bebf6f02ba5cdbeec2b030c55e45986afb49cba79d1b20afde62a4be3e5ca474a738e681d3bb3b1f91d2b8b43100c'
