17c4b2d476d33f89dc7692f57fa47fd9e19005e7
sidney zhang <sidazhang89@gmail.com>
2013-06-04 21:56:45 -0700

    Javacsripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+
+//= require_tree ../../../vendor/assets/javascripts/
 //= require_tree .
+
+
+
---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $(".container.message").on("keyup", "form",function(){
+    // var count = (140 - $(this).find(".message").val().length)
+    // $(this).find(".message-word-count").html(count + " Characters")
+  })
+});
